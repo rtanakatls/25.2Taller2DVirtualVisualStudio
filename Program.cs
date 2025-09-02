6d09339965369574fc1fcb0b537c8c63d0edb7a4
Ricardo Tanaka Terukina <rtanaka92@gmail.com>
2025-09-02 17:28:19 -0500

    Added examples

--- Program.cs
+++ Program.cs
@@ -40,7 +40,6 @@ namespace Taller2D252
 
             Console.WriteLine($"{d} días son {d/7} semanas");
             Console.WriteLine($"{d} días son {d/365} años");
-            */
 
             int year;
             Console.WriteLine("Ingrese un año:");
@@ -68,6 +67,143 @@ namespace Taller2D252
                 Console.WriteLine("El año no es bisiesto");
             }
 
+            int a;
+
+            Console.WriteLine("Ingrese un número:");
+            a = int.Parse(Console.ReadLine());
+            if (a > 0)
+            {
+                Console.WriteLine("El número es positivo");
+            }
+            else if (a < 0)
+            {
+                Console.WriteLine("El número es negativo");
+            }
+            else 
+            {
+                Console.WriteLine("El número es cero");
+            }
+            
+
+            int a;
+
+            Console.WriteLine("Ingrese un número:");
+            a = int.Parse(Console.ReadLine());
+            
+            if (a % 7 == 0 && a % 13 == 0)
+            {
+                Console.WriteLine("El número es divisible entre 7 y 13");
+            }
+            else if (a % 7 == 0)
+            {
+                Console.WriteLine("El número es divisible entre 7");
+            }
+            else if (a % 13 == 0)
+            {
+                Console.WriteLine("El número es divisible entre 13");
+            }
+            else
+            {
+                Console.WriteLine("El número no es divisible entre 7 ni 13");
+            }
+            
+
+            int a;
+
+            Console.WriteLine("Ingrese un número:");
+            a = int.Parse(Console.ReadLine());
+
+            if (a % 2 == 0)
+            {
+                Console.WriteLine("El número es par");
+            }
+            else
+            {
+                Console.WriteLine("El número es impar");
+            }
+            
+
+            float grade1, grade2, grade3, grade4;
+
+            Console.WriteLine("Ingrese la primera nota:");
+            grade1 = float.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese la segunda nota:");
+            grade2 = float.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese la tercera nota:");
+            grade3 = float.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese la nota de jurado:");
+            grade4 = float.Parse(Console.ReadLine());
+
+            float classAverage = (grade1 + grade2 + 2 * grade3) / 4;
+            float finalAverage = (2 * classAverage + grade4) / 3;
+
+            Console.WriteLine($"El promedio es: {finalAverage} ");
+            if (finalAverage >= 13)
+            {
+                Console.WriteLine("Aprobado");
+            }
+            else
+            {
+                Console.WriteLine("Desaprobado");
+            }
+
+            */
+
+            bool continueFlag = true;
+
+            while (continueFlag)
+            {
+                float a;
+                float b;
+                string operation;
+
+                Console.WriteLine("Ingrese el primer número:");
+                a = float.Parse(Console.ReadLine());
+                Console.WriteLine("Ingrese el segundo número:");
+                b = float.Parse(Console.ReadLine());
+
+                Console.WriteLine("Ingrese la operación (+, -, *, /, %) o salir:");
+                operation = Console.ReadLine();
+
+                switch (operation)
+                {
+                    case "+":
+                        Console.WriteLine($"{a} + {b} = {a + b}");
+                        break;
+                    case "-":
+                        Console.WriteLine($"{a} - {b} = {a - b}");
+                        break;
+                    case "*":
+                        Console.WriteLine($"{a} * {b} = {a * b}");
+                        break;
+                    case "/":
+                        if (b != 0)
+                        {
+                            Console.WriteLine($"{a} / {b} = {a / b}");
+                        }
+                        else
+                        {
+                            Console.WriteLine("No se puede dividir por cero");
+                        }
+                        break;
+                    case "%":
+                        if (b != 0)
+                        {
+                            Console.WriteLine($"{a} % {b} = {a % b}");
+                        }
+                        else
+                        {
+                            Console.WriteLine("No se puede dividir por cero");
+                        }
+                        break;
+                    case "salir":
+                        continueFlag = false;
+                        break;
+                    default:
+                        Console.WriteLine("Operación no válida");
+                        break;
+                }
+            }
 
 
             /*
